04c9d95955caf9c4aafca2f037d430a90483b0ce
Fabrice FACORAT <fabrice.facorat@gmail.com>
2011-10-21 12:01:51 +0200

    add setup.py fix W3C log format read

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='timegrep',
+      version='1.0',
+      description='Perform a binary search through a log file to find a range of times and print the corresponding lines',
+      author='Fabrice FACORAT',
+      author_email='fabrice.facorat@gmail.com',
+      url='https://github.com/linux-wizard',
+      scripts = ["timegrep"],
+      classifiers=[
+      'Development Status :: 4 - Beta',
+      'Environment :: Console',
+      'Intended Audience :: System Administrators',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: GNU General Public License (GPL)',
+      'Operating System :: POSIX :: Linux',
+      'Programming Language :: Python',
+      'Topic :: Utilities',
+      'Topic :: System :: Systems Administration'
+      ]
+     )
